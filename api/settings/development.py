73754d9dedcc3a37a859ba1c3077fcd5634a5c04
TonsV2 <sebastianthegreatful@gmail.com>
2026-02-01 02:49:50 +0700

    chore: add CORS urls for react development

--- api/settings/development.py
+++ api/settings/development.py
@@ -17,6 +17,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True  # Only for development
