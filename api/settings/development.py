8b1e8e0137bbef15f42c1ce3bc9716b5ac0cca9a
TonsV2 <sebastianthegreatful@gmail.com>
2026-02-13 22:09:14 +0700

    chore: Docker with Celery and Postgresql

--- api/settings/development.py
+++ api/settings/development.py
@@ -6,6 +6,14 @@ DEBUG = True
 # Development database
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": "5432",
+    },
+    "test": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
